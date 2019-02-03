f1d96f0e0a32805f1877c899c192e52cb29f3af4
bjones526 <bjones526@gmail.com>
2019-02-02 21:05:17 -0500

    semantic ui

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../App.scss";
 import { Container, Menu } from "semantic-ui-react";
-import "semantic-ui-css/semantic.min.css";
+import "../semantic/dist/semantic.min.css";
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
