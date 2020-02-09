390e0d98fe92b11b6d3371d0560b2d81abc22e83
bjones526 <bjones526@gmail.com>
2020-02-09 18:22:41 -0500

    content update

--- src/Components/About.js
+++ src/Components/About.js
@@ -17,7 +17,10 @@ class About extends Component {
                     </Grid>
                     <Grid item xs={12} sm={5} className="about-text">
                         <h1>About</h1>
-                        <p>I'm currently a senior developer at Fidelity Labs. Before that, I was a front end developer for IBM Hybrid Cloud. Most of my experience has been with React and Redux, but I've also developed in Angular and written backend apps with node express.</p>
+                        <p>I'm currently a senior developer at Fidelity. Before that, I was a front end developer for IBM Hybrid Cloud. 
+                            Most of my experience has been with React and Redux. I've also developed in Angular and Vue, and I've written backend apps with node express.</p>
+                        <p>Check out <a href="https://css-tricks.com/author/baileyjones/">my author page on css tricks</a> or my <a href="https://github.com/bjones526">github profile</a> 
+                            to get a sense of topics I'm interested in!</p>
                         <p>I graduated from UNC's master's in Information Science program in 2016.
                             My master's paper focused on thumbnails for archival collections online, which I presented at the 2017 Joint Conference for Digital Libraries in Toronto.
                 You can find the <a href='https://ieeexplore.ieee.org/document/7991599'> publication</a> in the JCDL conference proceedings on IEEE Xplore. </p>
